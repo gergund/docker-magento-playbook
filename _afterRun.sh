84c30435f4ef91c3a086974f10e0a74105389158
Denys Gergun <gergund@gmail.com>
2014-11-27 09:56:49 -0500

    Final

--- _afterRun.sh
+++ _afterRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/sleep 5
+/usr/bin/sleep 10
 
 /usr/bin/chown -R apache:apache /var/www/
 
@@ -11,8 +11,8 @@
 /usr/bin/mysql -e 'create database `magento-ee-1.14.1.0`';
 /usr/bin/mysql magento-ee-1.14.1.0 < /var/www/magento/shared/madison-island-ee.sql
 
-/usr/bin/php -f /var/www/magento/magento-ee-1.13.0.2/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.13.0.2" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
+/usr/bin/php -f /var/www/magento/magento-ee-1.13.0.2/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.13.0.2" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc/magento-ee-1.13.0.2/" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc/magento-ee-1.13.0.2/" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
 
-/usr/bin/php -f /var/www/magento/magento-ee-1.14.0.0/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.14.0.0" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
+/usr/bin/php -f /var/www/magento/magento-ee-1.14.0.0/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.14.0.0" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc/magento-ee-1.14.0.0/" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc/magento-ee-1.14.0.0/" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
 
-/usr/bin/php -f /var/www/magento/magento-ee-1.14.1.0/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.14.1.0" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
+/usr/bin/php -f /var/www/magento/magento-ee-1.14.1.0/install.php -- --license_agreement_accepted yes --skip_url_validation yes --locale en_US --timezone "America/Los_Angeles" --default_currency USD --db_host 127.0.0.1 --db_name "magento-ee-1.14.1.0" --db_user root --db_pass ""  --db_prefix magento_ --url "http://app.magento.loc/magento-ee-1.14.1.0/" --use_rewrites yes --use_secure yes --secure_base_url "http://app.magento.loc/magento-ee-1.14.1.0/" --use_secure_admin yes --admin_lastname admin --admin_firstname admin --admin_email "gergund@gmail.com" --admin_username admin --admin_password 123123qa --encryption_key ""
