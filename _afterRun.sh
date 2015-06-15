52b16e414a6a74c703aa4006e37c5a3dc03540e1
root <root@docker-hyp01-518815.slc01.dev.ebayc3.com>
2015-06-15 08:15:31 +0000

    Fixed path in _afterRun.sh

--- _afterRun.sh
+++ _afterRun.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-/usr/bin/sleep 5
-
-/usr/bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql
+/bin/sleep 5 && /bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql
