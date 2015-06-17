2eb63dd094507b3798cbc86163d23ec391556f10
root <root@p2986890.pubip.peer1.net>
2015-06-17 15:44:49 -0400

    Added maintenance.flag

--- _afterRun.sh
+++ _afterRun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/sleep 8 && /bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql
+touch /var/www/magento/maintenance.flag && /bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql && rm -Rf /var/www/magento/maintenance.flag
