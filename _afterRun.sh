fdf1494d3a3ec4d6eee647a9292acfaa758f52dd
root <root@p2986890.pubip.peer1.net>
2015-07-23 11:19:34 -0400

    added SSL and Mage Connect support

--- _afterRun.sh
+++ _afterRun.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 touch /var/www/magento/maintenance.flag && /bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql && rm -Rf /var/www/magento/maintenance.flag
+chmod +x /var/www/magento/mage && /var/www/magento/mage mage-setup
