c23ccf72de81b9167864a535d9367ca247728cb5
root <root@p2986890.pubip.peer1.net>
2015-07-23 12:41:55 -0400

    Fix

--- _afterRun.sh
+++ _afterRun.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 touch /var/www/magento/maintenance.flag && /bin/chown -R apache:apache /var/www/ && /usr/bin/mysql -e 'create database `magento`' && /usr/bin/mysql magento < /var/www/magento/magento.sql && rm -Rf /var/www/magento/maintenance.flag
-chmod +x /var/www/magento/mage && /var/www/magento/mage mage-setup
